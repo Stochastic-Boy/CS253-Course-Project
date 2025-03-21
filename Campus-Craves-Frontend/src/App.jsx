875f604b90a72621da70715a567c635135f76476
Sowmya Sri Chatla <104137234+KreyulTWSA@users.noreply.github.com>
2025-03-21 17:08:55 +0530

    Edited App.jsx to remove the separate login and sign-up pages for buyers and sellers, to to maintain a single signup and login component.  Added an additional path to the SellerStores page.
    Edited App.jsx to remove the separate login and sign-up pages for buyers and sellers, to to maintain a single signup and login component.  Added an additional path to the SellerStores page.

--- Campus-Craves-Frontend/src/App.jsx
+++ Campus-Craves-Frontend/src/App.jsx
@@ -4,15 +4,12 @@ import Menu from "./pages/customer/Menu"
 import Offers from "./pages/customer/Offers"
 import Canteens from "./pages/customer/Canteens"
 import Orders from "./pages/customer/Orders";
-import BuyerLoginPage from "./pages/customer/BuyerLoginPage";
-import BuyerSignUpPage from "./pages/customer/BuyerSignUpPage";
 import SignUp from "./components/SignUp";
 import LoginForm from "./components/LoginForm";
-import SellerLoginPage from "./pages/seller/SellerLoginPage";
-import SellerSignUpPage from "./pages/seller/SellerSignUpPage";
 import ProductsView from "./pages/seller/ProductsView";
 import OrdersView from "./pages/seller/OrdersView";
 import CategoriesView from "./pages/seller/CategoriesView";
+import SellerStores from "./pages/seller/SellerStores";
 
 const App = () => {
   return (
@@ -23,15 +20,12 @@ const App = () => {
         <Route path="/offers" element={<Offers />} />
         <Route path="/canteens" element={<Canteens />} />
         <Route path="/order" element={<Orders />} />
-        <Route path="/buyer-login" element={<BuyerLoginPage />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/seller-login" element={<SellerLoginPage />} />
-        <Route path="/seller-signup" element={<SellerSignUpPage />} />
-        <Route path="/buyer-signup" element={<BuyerSignUpPage />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/productsview" element={<ProductsView />} />
         <Route path="/ordersview" element={<OrdersView />} />
         <Route path="/categoriesview" element={<CategoriesView />} />
+        <Route path="/sellerstores" element={<SellerStores />} />
       </Routes>
     </Router>
   );
