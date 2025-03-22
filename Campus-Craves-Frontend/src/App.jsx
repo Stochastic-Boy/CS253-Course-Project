f519adb16390c2ef75a42b198d5f3d1698addd47
KreyulTWSA <sowmyasrichatla232@gmail.com>
2025-03-23 02:22:12 +0530

    Updated App.jsx to configure newly added pages

--- Campus-Craves-Frontend/src/App.jsx
+++ Campus-Craves-Frontend/src/App.jsx
@@ -5,7 +5,10 @@ import Offers from "./pages/customer/Offers"
 import Canteens from "./pages/customer/Canteens"
 import Orders from "./pages/customer/Orders";
 import SignUp from "./components/SignUp";
-import LoginForm from "./components/LoginForm";
+import Login from "./components/LoginForm";
+import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
+import ConfirmSignup from "./components/ConfirmSignup";
 import ProductsView from "./pages/seller/sellerview/ProductsView";
 import OrdersView from "./pages/seller/OrdersView";
 import CategoriesView from "./pages/seller/CategoriesView";
@@ -22,7 +25,10 @@ const App = () => {
         <Route path="/canteens" element={<Canteens />} />
         <Route path="/order" element={<Orders />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/login" element={<LoginForm />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/confirm-signup" element={<ConfirmSignup />} />
         <Route path="/admin/*" element={<SellerView/>} />
         <Route path="/sellerstores" element={<SellerStores />} />
       </Routes>
