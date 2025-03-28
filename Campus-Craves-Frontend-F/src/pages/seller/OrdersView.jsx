15731016d9a5aade2d95375e7505f991d6f3291e
Abhishek Kumar <abhikumar21@iitk.ac.in>
2025-03-28 20:45:54 +0530

    Orders View

--- Campus-Craves-Frontend-F/src/pages/seller/OrdersView.jsx
+++ Campus-Craves-Frontend-F/src/pages/seller/OrdersView.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import './Ordersview.css';
 
 const OrdersView = () => {
   const [orders, setOrders] = useState([]);
@@ -23,29 +24,32 @@ const OrdersView = () => {
     }
   };
 
-  const markDelivered = async (orderId) => {
-    try {
-      const res = await axios.post(`http://127.0.0.1:8000/orders/deliver/${orderId}/`, {}, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setMessage(res.data.message);
-      fetchSellerOrders();
-    } catch (err) {
-      console.error("Error marking delivered", err);
-    }
-  };
+  // const markDelivered = async (orderId) => {
+  //   try {
+  //     const res = await axios.post(`http://127.0.0.1:8000/orders/deliver/${orderId}/`, {}, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //     });
+  //     setMessage(res.data.message);
+  //     fetchSellerOrders();
+  //   } catch (err) {
+  //     console.error("Error marking delivered", err);
+  //   }
+  // };
 
   return (
     <div className="p-4">
       <h2 className="text-xl font-bold mb-4">Store Orders</h2>
       {message && <p className="text-green-600">{message}</p>}
+      <div className="seller-orders">
       {orders.map((order) => (
-        <div key={order.id} className="border rounded p-3 mb-4">
-          <p className="font-semibold">Order #{order.id}</p>
+        <div key={order.id} className="seller-orders-card border rounded p-3 mb-4">
+          <div className="orders-h1">
+            <p className="font-semibold">Order Number: {order.id}</p>
+            <h5 style={{color: order.status==='delivered' ? "green" : order.status==='pending' ? "rgb(242, 255, 0)" : "red"}}>{order.status}</h5>
+          </div>  
           <p>Buyer: {order.user}</p>
-          <p>Status: {order.status}</p>
           <p>Payment: {order.payment_method}</p>
           <p>Total: ₹{order.total_price}</p>
           <p>Address: {order.delivery_address}</p>
@@ -59,16 +63,17 @@ const OrdersView = () => {
             ))}
           </ul>
 
-          {order.status !== "delivered" && order.status !== "cancelled" && (
+          {/* {order.status !== "delivered" && order.status !== "cancelled" && (
             <button
               onClick={() => markDelivered(order.id)}
               className="mt-2 px-3 py-1 bg-green-600 text-white rounded"
             >
               Mark as Delivered
             </button>
-          )}
+          )} */}
         </div>
       ))}
+      </div>
     </div>
   );
 };
