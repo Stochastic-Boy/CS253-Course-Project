45121d5b7b8de0ca5a378357a641eb7ba2522615
Siddharth Pathak <110186062+Stochastic-Boy@users.noreply.github.com>
2025-04-02 03:29:42 +0530

    Delete Campus-Craves-Frontend-F directory

--- Campus-Craves-Frontend-F/src/reduxstore/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../reduxfeatures/userSlice';
-
-export const store = configureStore({
-  reducer: {
-    user: userReducer,  
-  },
-});
